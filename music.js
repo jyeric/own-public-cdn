2a92535a52e7c0acece1b34ec2a5865bec53f04e
jyeric <jyeric1@126.com>
2019-10-01 19:00:17 +0800

    Update music.js

--- music.js
+++ music.js
@@ -11,21 +11,21 @@ const ap = new APlayer({
         artist: 'Neru/鏡音リン/鏡音レン',
         url: 'http://music.163.com/song/media/outer/url?id=505474379.mp3',
         cover: 'https://i.loli.net/2019/06/07/5cf9abb8ca8ca95762.jpg',
-        lrc: 'https://od.lk/s/NzNfMTQ4MzE3NTdf/%E7%97%85%E5%90%8D%E3%81%AF%E6%84%9B%E3%81%A0%E3%81%A3%E3%81%9F.lrc',
+        lrc: 'https://likecoding.tech/music/%E7%97%85%E5%90%8D%E3%81%AF%E6%84%9B%E3%81%A0%E3%81%A3%E3%81%9F.lrcc',
       },
       {
         name: '达拉崩吧',
         artist: '洛天依、言和',
         url: 'https://od.lk/s/NzNfMTQyMTA5ODBf/%E8%BE%BE%E6%8B%89%E5%B4%A9%E5%90%A7-%E6%B4%9B%E5%A4%A9%E4%BE%9D.mp3',
         cover: 'https://s2.ax1x.com/2019/06/08/VByRI0.jpg',
-        lrc: 'https://od.lk/s/NzNfMTQ4MzE3NThf/%E8%BE%BE%E6%8B%89%E5%B4%A9%E5%90%A7-%E6%B4%9B%E5%A4%A9%E4%BE%9D%2C%E8%A8%80%E5%92%8C.lrc',
+        lrc: 'https://likecoding.tech/music/%E8%BE%BE%E6%8B%89%E5%B4%A9%E5%90%A7-%E6%B4%9B%E5%A4%A9%E4%BE%9D,%E8%A8%80%E5%92%8C.lrc',
       },
       {
         name: '膜你抄',
         artist: 'Menci/24OI',
         url: 'http://music.163.com/song/media/outer/url?id=459434585.mp3',
         cover: 'https://ae01.alicdn.com/kf/H349ed76e5e5148e4b4602bc958695d7d5.png',
-        lrc: 'https://od.lk/s/NzNfMTQ4MzE3NTRf/%E8%86%9C%E4%BD%A0%E6%8A%84.lrc',
+        lrc: 'https://likecoding.tech/music/%E8%86%9C%E4%BD%A0%E6%8A%84.lrc',
       }
     ]
 });
